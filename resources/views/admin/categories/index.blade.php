80ad37aae14829c3a7968e9a5589ae201ee07e1a
oookiooo <84783145+oookiooo@users.noreply.github.com>
2022-08-06 23:41:59 +0200

    Initial commit

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,58 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+
+
+        <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+            <a href="{{route('admin.categories.create')}}">
+            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Dodaj Kategorie</button>
+            </a>
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="py-3 px-6">
+                        Name
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Image
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Description
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Action
+                    </th>
+
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($categories as $category)
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{$category->name}}
+                    </td>
+                    <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                       <img src="{{Storage::url($category->image)}}" class="w-16 h-16 rounded">
+                        {{Storage::url($category->image)}}
+                    </td>
+                    <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{$category->description}}
+                    </td>
+                    <td class="py-4 px-6">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    </td>
+
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+</x-admin-layout>
